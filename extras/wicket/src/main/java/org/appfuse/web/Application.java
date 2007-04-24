27b7c4e94aca801abe7c026d2742cfe8c89b416e
Matt Raible <matt@raibledesigns.com>
2007-04-24 05:58:58 +0000

    Integrated Spring into Stripes and Wicket and removed java.net repo (since jta is now in central repo)

--- extras/wicket/src/main/java/org/appfuse/web/Application.java
+++ extras/wicket/src/main/java/org/appfuse/web/Application.java
@@ -1,8 +1,14 @@
 package org.appfuse.web;
 
 import wicket.protocol.http.WebApplication;
+import wicket.spring.injection.annot.SpringComponentInjector;
 
 public class Application extends WebApplication {
+    public void init() {
+        // notice in 2.0+ versions this is replaced by new SpringWicketModule(this);
+        addComponentInstantiationListener(new SpringComponentInjector(this));
+    }
+
     public Class getHomePage() {
         return Index.class;
     }
