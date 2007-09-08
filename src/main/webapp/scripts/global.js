b75f30b818edea7c9c876f348fc1f18bae1b61a1
Matt Raible <matt@raibledesigns.com>
2007-09-08 05:17:50 +0000

    Modified highlightTableRows to work with JSF

--- src/main/webapp/scripts/global.js
+++ src/main/webapp/scripts/global.js
@@ -2,24 +2,31 @@ function highlightTableRows(tableId) {
     var previousClass = null;
     var table = document.getElementById(tableId); 
     var tbody = table.getElementsByTagName("tbody")[0];
-    var rows = tbody.getElementsByTagName("tr");
+    var rows;
+    if (tbody == null) {
+        rows = table.getElementsByTagName("tr");
+    } else {
+        rows = tbody.getElementsByTagName("tr");
+    }
     // add event handlers so rows light up and are clickable
     for (i=0; i < rows.length; i++) {
         rows[i].onmouseover = function() { previousClass=this.className;this.className+=' over' };
         rows[i].onmouseout = function() { this.className=previousClass };
         rows[i].onclick = function() {
             var cell = this.getElementsByTagName("td")[0];
-            if (cell.getElementsByTagName("a").length > 0) {
-                var link = cell.getElementsByTagName("a")[0];
-                if (link.onclick) {
-                    call = link.getAttributeValue("onclick");
-                    // this will not work for links with onclick handlers that return false
-                    eval(call);
-                } else {
-                  location.href = link.getAttribute("href");
-                }
-                this.style.cursor="wait";
+            var link = cell.getElementsByTagName("a")[0];
+            if (link.onclick) {
+                call = link.getAttribute("onclick");
+                if (call.indexOf("return ") == 0) {
+                    call = call.substring(7);
+                } 
+                // this will not work for links with onclick handlers that return false
+                eval(call);
+            } else {
+                location.href = link.getAttribute("href");
             }
+            this.style.cursor="wait";
+            return false;
         }
     }
 }
@@ -29,4 +36,4 @@ window.defaultStatus=document.title;
 
 // The following doesn't work in IE 6 or 7
 // window.onload = StyleSheetSwitcher.initialize();
-StyleSheetSwitcher.initialize();
+window.onload = function() { StyleSheetSwitcher.initialize(); }
