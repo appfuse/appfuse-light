0000ed56b2d237b0f85f01355453829b98fd8f8b
Matt Raible <matt@raibledesigns.com>
2011-04-04 07:36:07 +0000

    Turn off gzip compression. Doesn't work on demo2.appfuse.org.

--- tapestry/src/main/java/org/appfuse/webapp/services/AppModule.java
+++ tapestry/src/main/java/org/appfuse/webapp/services/AppModule.java
@@ -2,6 +2,7 @@ package org.appfuse.webapp.services;
 
 import org.apache.tapestry5.SymbolConstants;
 import org.apache.tapestry5.ioc.annotations.Symbol;
+import org.apache.tapestry5.ioc.MappedConfiguration;
 import org.apache.tapestry5.services.ComponentSource;
 import org.apache.tapestry5.services.ExceptionReporter;
 import org.apache.tapestry5.services.RequestExceptionHandler;
@@ -18,6 +19,10 @@ import java.io.IOException;
  * @version $Id: AppModule.java 5 2008-08-30 09:59:21Z serge.eby $
  */
 public class AppModule {
+
+    public static void contributeApplicationDefaults(MappedConfiguration<String, String> configuration) {
+        configuration.add(SymbolConstants.GZIP_COMPRESSION_ENABLED, "false");
+    }
 
     /**
      * Decorate Error page
