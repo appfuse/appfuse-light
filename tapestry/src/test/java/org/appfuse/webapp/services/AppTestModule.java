9e7c3a86c32156933841aad1b2a9ca1c2306c06f
Matt Raible <matt@raibledesigns.com>
2014-12-04 13:01:57 -0700

    APF-1454: Optimize imports.

--- tapestry/src/test/java/org/appfuse/webapp/services/AppTestModule.java
+++ tapestry/src/test/java/org/appfuse/webapp/services/AppTestModule.java
@@ -1,11 +1,8 @@
 package org.appfuse.webapp.services;
 
-import org.apache.tapestry5.ioc.MappedConfiguration;
 import org.apache.tapestry5.ioc.OrderedConfiguration;
 import org.apache.tapestry5.ioc.annotations.ImportModule;
-import org.apache.tapestry5.ioc.annotations.SubModule;
 import org.apache.tapestry5.services.*;
-import org.appfuse.Constants;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 
@@ -27,14 +24,14 @@ public class AppTestModule {
                                                 final RequestGlobals requestGlobals) {
         RequestFilter filter = new RequestFilter() {
             public boolean service(Request request, Response response, RequestHandler handler)
-                    throws IOException {
+                throws IOException {
                 requestGlobals.storeServletRequestResponse(
-                        new MockHttpServletRequest(),
-                        new MockHttpServletResponse());
+                    new MockHttpServletRequest(),
+                    new MockHttpServletResponse());
 
                 return handler.service(request, response);
             }
         };
-        config.add("SpringMockHttpRequestAndResponse", filter, "after:CheckForUpdates","before:URLRewriter");
+        config.add("SpringMockHttpRequestAndResponse", filter, "after:CheckForUpdates", "before:URLRewriter");
     }
 }
